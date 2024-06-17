8c5a23df0c7552226d2fb4a3b6d71798bda3712e
wallacemiller <wallacemillerdias@gmail.com>
2024-06-17 16:32:03 -0300

    :sparkles: Operações Basicas

---
+++ src/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,58 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        Item item = new Item(nome, preco, quantidade);
+        itemList.add(item);
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itensParaRemover = new ArrayList<>();
+        if (!itemList.isEmpty()) {
+            for (Item i : itemList) {
+                if (i.getNome().equalsIgnoreCase(nome)) {
+                    itensParaRemover.add(i);
+                }
+            }
+            itemList.removeAll(itensParaRemover);
+        } else {
+            System.out.println("A lista está vazia");
+        }
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0;
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                valorTotal += item.getPreco() * item.getQuantidade();
+            }
+            return valorTotal;
+        } else {
+            throw new RuntimeException("A lista está vazia");
+        }
+    }
+
+    public void exibirItens() {
+        if (!itemList.isEmpty()) {
+            System.out.println(this.itemList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CarrinhoDeCompras{" +
+                "itemList=" + itemList +
+                '}';
+    }
+}
