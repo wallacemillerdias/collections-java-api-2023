bc14e95dca26527b683a501396bd86fb1af0b31c
wallacemiller <wallacemillerdias@gmail.com>
2024-06-26 12:04:14 -0300

    :sparkles: Update Agenda Contatos

---
+++ src/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,46 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatosSet;
+
+    public AgendaContatos() {
+        this.contatosSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatosSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos() {
+        System.out.println(contatosSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for (Contato contato : contatosSet) {
+            if (contato.getNome().startsWith(nome)) {
+                contatosPorNome.add(contato);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        if (!contatosSet.isEmpty()) {
+            for (Contato c : contatosSet) {
+                if (c.getNome().equalsIgnoreCase(nome)) {
+                    c.setNumero(novoNumero);
+                    contatoAtualizado = c;
+                    break;
+                }
+            }
+            return contatoAtualizado;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+}
