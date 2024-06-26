ae655544cf6e44c93b9e2db1e9b458e812ffec00
wallacemiller <wallacemillerdias@gmail.com>
2024-06-26 12:05:31 -0300

    :sparkles: Update

--- src/set/TestSet.java
+++ src/set/TestSet.java
@@ -1,6 +1,9 @@
 package set;
 
 import set.OperacoesBasicas.ConjuntoConvidados;
+import set.OperacoesBasicas.ConjuntoPalavrasUnicas;
+import set.Pesquisa.AgendaContatos;
+import set.Pesquisa.Contato;
 
 public class TestSet {
     public static void main(String[] args) {
@@ -25,5 +28,40 @@ public class TestSet {
 
         System.out.println("Convidados no conjunto após a remoção:");
         conjuntoConvidados.exibirConvidados();
+
+        // Conjunto Palavras Unicas
+        ConjuntoPalavrasUnicas conjuntoPalavras = new ConjuntoPalavrasUnicas();
+
+        conjuntoPalavras.adicionarPalavra("bola");
+        conjuntoPalavras.adicionarPalavra("bala");
+        conjuntoPalavras.adicionarPalavra("bola");
+        conjuntoPalavras.adicionarPalavra("bolacha");
+
+        conjuntoPalavras.exibirPalavrasUnicas();
+
+        conjuntoPalavras.removerPalavra("bola");
+        conjuntoPalavras.exibirPalavrasUnicas();
+
+        System.out.println(conjuntoPalavras.verificarPalavra("bala"));
+
+        // Agenda Contatos
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Wallace", 992447898);
+        agendaContatos.adicionarContato("Wallace Miller", 998981055);
+        agendaContatos.adicionarContato("João", 991223654);
+        agendaContatos.adicionarContato("Denise", 999977444);
+        agendaContatos.adicionarContato("Jean", 992447898);
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Denise"));
+
+        Contato contatoAtualizado = agendaContatos.atualizarNumeroContato("jean", 999999999);
+        System.out.println("Contato atualizado: " + contatoAtualizado);
+
+        System.out.println("Contatos na agenda após atualização:");
+        agendaContatos.exibirContatos();
+
     }
 }
