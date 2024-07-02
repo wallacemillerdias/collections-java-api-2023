d44b485a2309e0738a7189f97cef681337394a20
wallacemiller <wallacemillerdias@gmail.com>
2024-07-02 10:06:28 -0300

    :loud_sound: Update Tarefa

---
+++ src/set/Pesquisa/Tarefa.java
@@ -0,0 +1,32 @@
+package set.Pesquisa;
+
+public class Tarefa {
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + descricao + " - " + concluida + '}';
+    }
+}
