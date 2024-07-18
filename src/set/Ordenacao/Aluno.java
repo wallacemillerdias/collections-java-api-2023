881b3e5a48031f072d61d5ea438bb7c2a928250f
wallacemiller <wallacemillerdias@gmail.com>
2024-07-18 10:48:47 -0300

    :sparkles: Add class Aluno

---
+++ src/set/Ordenacao/Aluno.java
@@ -0,0 +1,62 @@
+package set.Ordenacao;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Aluno implements Comparable<Aluno> {
+    private String nome;
+    private Long matricula;
+    private double media;
+
+    public Aluno(String nome, Long matricula, double media) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.media = media;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(matricula, aluno.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(matricula);
+    }
+
+    @Override
+    public int compareTo(Aluno aluno) {
+        return nome.compareTo(aluno.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", matricula=" + matricula +
+                ", media=" + media +
+                '}';
+    }
+}
+class ComparatorNota implements Comparator<Aluno> {
+
+    @Override
+    public int compare(Aluno nota1, Aluno nota2) {
+        return Double.compare(nota1.getMedia(), nota2.getMedia());
+    }
+}
