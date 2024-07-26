e146a77191cd34bc2762060bd1723a8f912a3937
wallacemiller <wallacemillerdias@gmail.com>
2024-07-26 17:44:47 -0300

    :sparkles: Word Count

---
+++ src/map/Pesquisa/ContagemPalavras.java
@@ -0,0 +1,44 @@
+package map.Pesquisa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContagemPalavras {
+    private Map<String, Integer> palavrasMap;
+
+    public ContagemPalavras() {
+        this.palavrasMap = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, Integer contagem) {
+        palavrasMap.put(palavra, contagem);
+    }
+
+    public void removerPalavra(String palavra) {
+        if (!palavrasMap.isEmpty()) {
+            palavrasMap.remove(palavra);
+        } else {
+            System.out.println("O Map está vazio!");
+        }
+    }
+
+    public int exibirContagemPalavras() {
+        int contagem = 0;
+        for (int p : palavrasMap.values()) {
+            contagem += p;
+        }
+        return contagem;
+    }
+
+    public String encontrarPalavraMaisFrequente() {
+        String palavraMaisFrequente = null;
+        int maiorContagem = 0;
+        for (Map.Entry<String, Integer> entry : palavrasMap.entrySet()) {
+            if (entry.getValue() > maiorContagem) {
+                maiorContagem = entry.getValue();
+                palavraMaisFrequente = entry.getKey();
+            }
+        }
+        return palavraMaisFrequente;
+    }
+}
